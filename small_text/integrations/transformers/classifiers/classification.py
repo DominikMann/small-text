300b930a1bda3214c6167a40b0baba1ad3f2bb9e
DominikMann <105874983+DominikMann@users.noreply.github.com>
2023-12-18 16:43:03 +0100

    Update classification.py

--- small_text/integrations/transformers/classifiers/classification.py
+++ small_text/integrations/transformers/classifiers/classification.py
@@ -597,7 +597,9 @@ class TransformerBasedClassification(TransformerBasedEmbeddingMixin, PytorchClas
                          f'\tTrain Set Size: {len(sub_train)}\n'
                          f'\tLoss: {train_loss:.4f}(train)\t|\tAcc: {train_acc * 100:.1f}%(train)'
                          f'{valid_loss_txt}',
-                         verbosity=VERBOSITY_MORE_VERBOSE)
+                         verbosity=VERBOSITY_MORE_VERBOSE)#
+        print(f'Train Loss: {train_loss}'
+        ürint(f'Val Loss: {valid_loss}')
 
     def validate(self, validation_set):
 
